da712b1926317f4b2d657cbc11f4ffbc5434c9f0
Crend King <975235+CrendKing@users.noreply.github.com>
2024-07-02 14:38:43 -0700

    Initial commit for fork

--- README.md
+++ README.md
@@ -1,50 +1,16 @@
-# "Recently Opened" Cleaner
+This extension removes the recently opened entries that no longer valid. Surprisingly such basic functionality is not yet provided out-of-box.
 
-This extension allows you to clean up the "Recently Opened" list in VS Code.
+It specifically only targets local file-system entries. Other entry types such as "Virtual Workspace" and "SSH Remote" are ignored.
 
-## Intro
+A new command "Sweep non-existent recently opened entries" is added, which is also exexcuted automatically upon startup.
 
-The "File: Open Recent" command is a handy feature provided by VS Code. It allows to open recently used workspaces and files through a dropdown menu right in the editor.
+### Notes about the fork
 
-Unfortunately, it is not quite flawless. See, every workspace listed in the dropdown is associated with a particular path on local disk. That's fine as long as workspaces are not moved or deleted. But this happens a lot in practice. Maybe some of the workspaces opened were only temporary to begin with. Or maybe they were created in the wrong location. Either way, over time, the list of recently opened workspaces will just grow and be filled with useless entries.
+Forked from [vsc-recently-opened-cleaner](https://github.com/WissenIstNacht/vsc-recently-opened-cleaner). Originally deviates in the following aspects:
 
-To remediate this issue, this extension provides a simple command that cleans the list. To be more precise, the command goes through each workspace in the list and checks if the workspace's path is still valid. If that's not the case that entry will be removed form the list.
+1. The original only operates on the workspaces and folders. This extension works on file entries as well.
+2. The original provides the setting to execute the command at startup, and defaults to OFF. This extension makes the setting default to ON as an opinionated choice.
 
-## Features
+### Attribution
 
-### Clean Recently Opened List
-
-Currently, the extension comes with one command: `Clean Recently Opened List`. This command allows you to remove all the entries from the list that do not point to a valid path in one go.
-
-Additionally, the the extension can run this command automatically once vs code is done starting up. The command will run without user interaction, except to report success or failure. This behaviour is turned off by default and can be activated in the settings.
-
-## Bugs & Wishes
-
-### Feature Requests
-
-I'm planning on improving the UX of the existing command (e.g., give feedback on the process or report errors). I might also add a couple of options like automatically triggering the command on application start, if desired. If there’s something that would particularly help you, drop and issue on Github!
-
-### Issues
-
-I plan on making this a bug-free place. But hey, they're tricky to get rid of. If you find any, drop and issue on Github! :)
-
-## Acknowledgements
-
-- Extension icon: [Dust icons created by Freepik - Flaticon](https://www.flaticon.com/free-icons/dust)
-
----
-
-## Development
-
-### Run the extension in VS Code
-
-Launch debug environment (default `F5`) to open a new window with the extension loaded. The opened window can be relaoded to include the latest updates to the code.
-
-### Run tests
-
-- Open the debug viewlet (`Ctrl+Shift+D` or `Cmd+Shift+D` on Mac) and from the launch configuration dropdown pick `Extension Tests`.
-- Press `F5` to run the tests in a new window with your extension loaded.
-- See the output of the test result in the debug console.
-- Make changes to `src/test/suite/extension.test.ts` or create new test files inside the `test/suite` folder.
-  - The provided test runner will only consider files matching the name pattern `**.test.ts`.
-  - You can create folders inside the `test` folder to structure your tests any way you want.
+Extension icon: [Housework icons created by Freepik - Flaticon](https://www.flaticon.com/free-icons/housework)
--- src/extension.ts
+++ src/extension.ts
@@ -1,173 +1,64 @@
-import * as fs from 'fs';
-import { ExtensionContext, Uri, commands, window, workspace } from 'vscode';
+'use strict'
 
-type URI = Uri;
+import * as fs from 'fs'
+import * as vscode from 'vscode'
 
-// The following interfaces are copied from
-// https://github.com/microsoft/vscode/blob/9ff83d41a3268ec578c032ae54102285b447e947/src/vs/platform/workspaces/common/workspaces.ts#L48
 export interface IRecentlyOpened {
-  workspaces: IRecentFolder[];
-  files: IRecentFile[];
+    workspaces: IRecentWorkspace[]
+    files: IRecentFile[]
 }
 
-export type IRecent = IRecentFolder | IRecentFile;
-
-interface IRecentFolder {
-  readonly folderUri: URI;
-  label?: string;
-  readonly remoteAuthority?: string;
-}
-
-interface IRecentFile {
-  readonly fileUri: URI;
-  label?: string;
-  readonly remoteAuthority?: string;
-}
-
-export function activate(context: ExtensionContext) {
-  let disposable = commands.registerCommand(
-    'recently-opened-cleaner.clean-recently-opened',
-    async () => {
-      let recentEntries = (await commands.executeCommand(
-        '_workbench.getRecentlyOpened'
-      )) as IRecentlyOpened;
-
-      const filtered = recentEntries.workspaces.filter(
-        (ws) => !isRecentlyOpenedValid(ws.folderUri)
-      );
-      const initialEntriesCount = recentEntries.workspaces.length;
-      const toDeleteCount = filtered.length;
-
-      if (toDeleteCount === 0) {
-        window.showInformationMessage(
-          'No stale items found in the recently opened list.'
-        );
-        return;
-      }
-
-      const promptValue = await window.showInformationMessage(
-        `Found ${toDeleteCount} stale item(s) in the recently opened list.`,
-        { modal: true },
-        ...['Delete']
-      );
-      console.log(promptValue);
-
-      if (!promptValue) {
-        window.showInformationMessage('Canceled deletion.');
-        return;
-      }
-
-      filtered.forEach((ws) => {
-        console.log('Deleting ' + ws.folderUri.toString());
-        try {
-          commands.executeCommand(
-            'vscode.removeFromRecentlyOpened',
-            ws.folderUri.fsPath
-          );
-        } catch (e) {
-          console.error(
-            'something went wrong for the folder with URI: ' +
-              ws.folderUri +
-              ' The error returned: ' +
-              e
-          );
-        } finally {
-          console.log('Deletion done');
-        }
-      });
-
-      recentEntries = (await commands.executeCommand(
-        '_workbench.getRecentlyOpened'
-      )) as IRecentlyOpened;
-      const newEntriesCount = recentEntries.workspaces.length;
-
-      if (initialEntriesCount - toDeleteCount === newEntriesCount) {
-        window.showInformationMessage(
-          `Successfully deleted ${toDeleteCount} stale item(s) from the recently opened list.`
-        );
-      } else {
-        window.showErrorMessage(
-          `Something went wrong. Out of the ${toDeleteCount} item(s) that should have been deleted, only ${
-            initialEntriesCount - newEntriesCount
-          } got removed.`
-        );
-      }
+type IRecentWorkspace = {
+    readonly folderUri: vscode.Uri
+} | {
+    readonly workspace: {
+        configPath: vscode.Uri
     }
-  );
-  context.subscriptions.push(disposable);
+}
 
-  disposable = commands.registerCommand(
-    'recently-opened-cleaner.automatically-clean-recently-opened',
-    async () => {
-      let recentEntries = (await commands.executeCommand(
-        '_workbench.getRecentlyOpened'
-      )) as IRecentlyOpened;
+type IRecentFile = {
+    readonly fileUri: vscode.Uri
+}
 
-      const filtered = recentEntries.workspaces.filter(
-        (ws) => !isRecentlyOpenedValid(ws.folderUri)
-      );
-      const initialEntriesCount = recentEntries.workspaces.length;
-      const toDeleteCount = filtered.length;
+export function activate(context: vscode.ExtensionContext) {
+    const commandName = 'recently-opened-sweeper.sweep'
 
-      if (toDeleteCount === 0) {
-        window.showInformationMessage(
-          'No stale items found in the recently opened list.'
-        );
-        return;
-      }
+    const disposable = vscode.commands.registerCommand(commandName, async () => {
+        // The "_workbench.getRecentlyOpened" is a private API.
+        // This issue https://github.com/microsoft/vscode/issues/124577 proposes to turn it public, which still hasn't happened yet
+        const recentlyOpened = await vscode.commands.executeCommand<IRecentlyOpened>('_workbench.getRecentlyOpened')
+        const toDelete = []
 
-      filtered.forEach((ws) => {
-        console.log('Deleting ' + ws.folderUri.toString());
-        try {
-          commands.executeCommand(
-            'vscode.removeFromRecentlyOpened',
-            ws.folderUri.fsPath
-          );
-        } catch (e) {
-          console.error(
-            'something went wrong for the folder with URI: ' +
-              ws.folderUri +
-              ' The error returned: ' +
-              e
-          );
+        for (const ws of recentlyOpened.workspaces) {
+            let uri
+            if ('folderUri' in ws) {
+                uri = ws.folderUri
+            } else if ('workspace' in ws) {
+                uri = ws.workspace.configPath
+            }
+
+            if (uri && uri.scheme === 'file' && !fs.existsSync(uri.fsPath)) {
+                toDelete.push(uri.fsPath)
+            }
         }
-      });
 
-      let updatedRecentEntries = (await commands.executeCommand(
-        '_workbench.getRecentlyOpened'
-      )) as IRecentlyOpened;
-      const updatedEntriesCount = updatedRecentEntries.workspaces.length;
+        for (const file of recentlyOpened.files) {
+            const path = file.fileUri.fsPath
+            if (!fs.existsSync(path)) {
+                toDelete.push(path)
+            }
+        }
 
-      if (initialEntriesCount - toDeleteCount === updatedEntriesCount) {
-        window.showInformationMessage(
-          `Successfully deleted ${toDeleteCount} stale item(s) from the recently opened list.`
-        );
-      } else {
-        window.showErrorMessage(
-          `Something went wrong. Out of the ${toDeleteCount} item(s) that should have been deleted, only ${
-            initialEntriesCount - updatedEntriesCount
-          } got removed.`
-        );
-      }
+        for (const path of toDelete) {
+            vscode.commands.executeCommand('vscode.removeFromRecentlyOpened', path)
+        }
+    })
+    context.subscriptions.push(disposable)
+
+    const config = vscode.workspace.getConfiguration('recently-opened-sweeper');
+    if (config.get('runAtStartup')) {
+        vscode.commands.executeCommand(commandName)
     }
-  );
-  context.subscriptions.push(disposable);
-  const config = workspace.getConfiguration('recently-opened-cleaner');
-  let shouldExecuteOnStart = config.get('runOnStartupFinished') as
-    | string
-    | undefined;
-  if (shouldExecuteOnStart) {
-    commands.executeCommand(
-      'recently-opened-cleaner.automatically-clean-recently-opened'
-    );
-  } else {
-    console.log('Recenty Opened Cleaner not enabled on start-up finished');
-  }
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
-
-function isRecentlyOpenedValid(uri: URI) {
-  return uri.scheme === 'vscode-vfs' || fs.existsSync(uri.fsPath);
-}
+export function deactivate() { }
