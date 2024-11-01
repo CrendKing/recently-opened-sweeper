d1db5d2ae8be56ccc4cbcf2cf8a227ff8e49b0a8
Crend King <975235+CrendKing@users.noreply.github.com>
2024-10-31 18:27:39 -0700

    Update keep count when configuration changes

--- src/extension.ts
+++ src/extension.ts
@@ -20,49 +20,57 @@ type IRecentFile = {
 
 export function activate(context: vscode.ExtensionContext) {
     const extensionName = 'recentlyOpenedSweeper'
-    const config = vscode.workspace.getConfiguration(extensionName)
-    const keepCount = config.get<number>('keepEntries.count', -1)
+    const keepCountConfigName = 'keepEntries.count'
     const commandName = `${extensionName}.sweep`
 
-    const disposable = vscode.commands.registerCommand(commandName, async function () {
-        // The "_workbench.getRecentlyOpened" is a private API.
-        // This issue https://github.com/microsoft/vscode/issues/124577 proposes to turn it public, which still hasn't happened yet
-        const recentlyOpened = await vscode.commands.executeCommand<IRecentlyOpened>('_workbench.getRecentlyOpened')
-        const toDelete = []
+    const config = vscode.workspace.getConfiguration(extensionName)
+    let keepCount = config.get<number>(keepCountConfigName, -1)
 
-        let validCount = 0
-        for (const ws of recentlyOpened.workspaces) {
-            let uri
-            if ('folderUri' in ws) {
-                uri = ws.folderUri
-            } else if ('workspace' in ws) {
-                uri = ws.workspace.configPath
+    context.subscriptions.push(
+        vscode.commands.registerCommand(commandName, async function () {
+            // The "_workbench.getRecentlyOpened" is a private API.
+            // This issue https://github.com/microsoft/vscode/issues/124577 proposes to turn it public, which still hasn't happened yet
+            const recentlyOpened = await vscode.commands.executeCommand<IRecentlyOpened>('_workbench.getRecentlyOpened')
+            const toDelete = []
+
+            let validCount = 0
+            for (const ws of recentlyOpened.workspaces) {
+                let uri
+                if ('folderUri' in ws) {
+                    uri = ws.folderUri
+                } else if ('workspace' in ws) {
+                    uri = ws.workspace.configPath
+                }
+
+                if (uri) {
+                    if ((keepCount >= 0 && validCount >= keepCount) || (uri.scheme === 'file' && !fs.existsSync(uri.fsPath))) {
+                        toDelete.push(uri.fsPath)
+                    } else {
+                        validCount += 1
+                    }
+                }
             }
 
-            if (uri) {
-                if ((keepCount >= 0 && validCount >= keepCount) || (uri.scheme === 'file' && !fs.existsSync(uri.fsPath))) {
-                    toDelete.push(uri.fsPath)
+            validCount = 0
+            for (const file of recentlyOpened.files) {
+                const path = file.fileUri.fsPath
+                if ((keepCount >= 0 && validCount >= keepCount) || !fs.existsSync(path)) {
+                    toDelete.push(path)
                 } else {
                     validCount += 1
                 }
             }
-        }
 
-        validCount = 0
-        for (const file of recentlyOpened.files) {
-            const path = file.fileUri.fsPath
-            if ((keepCount >= 0 && validCount >= keepCount) || !fs.existsSync(path)) {
-                toDelete.push(path)
-            } else {
-                validCount += 1
+            for (const path of toDelete) {
+                vscode.commands.executeCommand('vscode.removeFromRecentlyOpened', path)
             }
-        }
-
-        for (const path of toDelete) {
-            vscode.commands.executeCommand('vscode.removeFromRecentlyOpened', path)
-        }
-    })
-    context.subscriptions.push(disposable)
+        }),
+        vscode.workspace.onDidChangeConfiguration(evt => {
+            if (evt.affectsConfiguration(`${extensionName}.${keepCountConfigName}`)) {
+                keepCount = vscode.workspace.getConfiguration(extensionName).get<number>(keepCountConfigName, -1)
+            }
+        }),
+    )
 
     if (config.get<boolean>('runAtStartup.enabled', true)) {
         vscode.commands.executeCommand(commandName)
